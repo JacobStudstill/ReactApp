51e0176a544e7f0f89c29b83144369e9865a230b
JacobStudstill <jacob.studstill@gmail.com>
2023-05-01 14:27:54 -0400

    Added a switch case and use effect

--- src/Content.js
+++ src/Content.js
@@ -1,26 +1,67 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+
 
 const Content = () => {
     const [name, setName] = useState('Jacob')
     const [age, setAge] = useState('26')
     const [location, setLocation] = useState('FL')
+    const [header, setHeader] = useState('Im confused')
 
-    const onClick= () => {
-        setName('Jacob Studstill')
+    const [drink, setDrink] = useState('water')
+    const [drinkInfo, setDrinkInfo] = useState('Water is pretty refreshing')
+
+    const onClickGatorade = () => {
+        setDrink('gatorade')
+    }
+
+    const onClickSoda = () => {
+        setDrink('soda')
+    }
+
+    const onClickProteinShake = () => {
+        setDrink('protein shake')
+    }
+
+    const onClick = () => {
+        setName('no name')
         setAge('almost 27')
         setLocation('Orlando, FL')
     }
 
+    switch (drink) {
+        case 'gatorade': {
+            console.log('Gatorade is good on a hot day')
+            break
+        }
+        case 'soda': {
+            console.log('Soda is good when you need something sweet')
+            break
+        }
+        case 'protein shake': {
+            console.log('Protein shakes are good if you need protein')
+            break
+        }
+    }
+
+    useEffect(() => {
+        console.log(header)
+        setHeader('Working with useState and useEffect')
+    }, [header])
 
     return (
         <div className="pageContent">
-            <h2> Reviewing and Understanding React Hooks</h2>
-            <p> Hi, my name is { name }. I am { age } years old and I reside in { location }. </p>
+            <h2> {header} </h2>
+            <p> Hi, my name is {name}. I am {age} years old and I reside in {location}. </p>
             <button onClick={onClick}>Click here to see the code using useState</button>
+            <p>My favorite drink is water, what is yours?</p>
 
+            <button onClick={onClickGatorade}>Gatorade</button>
+            <button onClick={onClickSoda}>Soda</button>
+            <button onClick={onClickProteinShake}>Protein Shake</button>
+
+            <p>Check console log for more info</p>
         </div>
     )
 }
 
-export default Content
+export default Content;
