d0c59b86b064a38671c08fd1f192fdeeaeb55e87
JacobStudstill <jacob.studstill@gmail.com>
2023-05-01 13:10:57 -0400

    Switched repos since last one wouldn't push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Content from './Content';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          < Content />
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
